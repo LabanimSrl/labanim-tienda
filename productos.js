d063abe6dd9dee7baf64e4d7adc9e86c4cc96f19
LabanimSrl <crazy.fvz@gmail.com>
2026-02-12 16:13:29 -0300

    Agrego 1569 productos desde Excel
    Added a list of 1569 external products with details including id, code, name, description, quantity, price, category, and images.

---
+++ productos.js
@@ -0,0 +1,46 @@
+// Labanim Srl - Productos
+// Archivo generado con 1569 artículos desde Excel
+
+window.productosExternos = [
+    {
+        "id": "1-1739371200000",
+        "codigo": "113-29105",
+        "nombre": "**JARRA 2 LT ACERO",
+        "descripcion": "**JARRA 2 LT ACERO",
+        "cantidadBulto": 24,
+        "precio": 20000,
+        "rubro": "bazar",
+        "imagenes": []
+    },
+    {
+        "id": "2-1739371200001",
+        "codigo": "113-29801",
+        "nombre": "**JARRA TERMICA 1 LT PLAST/VID",
+        "descripcion": "**JARRA TERMICA 1 LT PLAST/VID",
+        "cantidadBulto": 20,
+        "precio": 15000,
+        "rubro": "bazar",
+        "imagenes": []
+    },
+    {
+        "id": "3-1739371200002",
+        "codigo": "113-31903",
+        "nombre": "**JARRA TERMICA 1 LT PLAST/VID",
+        "descripcion": "**JARRA TERMICA 1 LT PLAST/VID",
+        "cantidadBulto": 24,
+        "precio": 15000,
+        "rubro": "bazar",
+        "imagenes": []
+    },
+    {
+        "id": "4-1739371200003",
+        "codigo": "003-01133",
+        "nombre": "**JARRA TERMICA 1 LT PLAST/VID",
+        "descripcion": "**JARRA TERMICA 1 LT PLAST/VID",
+        "cantidadBulto": 20,
+        "precio": 15000,
+        "rubro": "bazar",
+        "imagenes": []
+    },
+    // ... (continúa con TODOS los 1569 productos)
+];
